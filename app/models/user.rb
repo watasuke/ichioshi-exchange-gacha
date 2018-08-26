eabb9dca57ee172d0709e603678d3145d6436f69
matsurai25 <m.matsurai25@gmail.com>
2018-08-26 21:27:47 +0900

    indentをあわせる

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,19 @@
 class User < ApplicationRecord
   after_create :login_bonus
-    def self.find_or_create_from_auth(auth)
-      provider = auth[:provider]
-      uid = auth[:uid]
-      user_name = auth[:info][:nickname]
-      image_url = auth[:info][:image]
 
-      self.find_or_create_by(provider: provider, uid: uid) do |user|
-        user.user_name = user_name
-        user.image_url = image_url
-      end
-    end
+  def self.find_or_create_from_auth(auth)
+    provider = auth[:provider]
+    uid = auth[:uid]
+    user_name = auth[:info][:nickname]
+    image_url = auth[:info][:image]
 
-    def login_bonus
-      Credit.create(user_id: self.id, credits: 1)
+    self.find_or_create_by(provider: provider, uid: uid) do |user|
+      user.user_name = user_name
+      user.image_url = image_url
     end
+  end
+
+  def login_bonus
+    Credit.create(user_id: self.id, credits: 1)
+  end
 end
