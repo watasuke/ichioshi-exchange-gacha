d967f8e25858a65802f0cb5c541e55450d26c4a2
matsurai25 <m.matsurai25@gmail.com>
2018-08-30 04:32:45 +0900

    pluckを使って書き換える

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,7 @@
 class UsersController < ApplicationController
   def collection
     results = Result.where(user_id: current_user.id)
-    collection_ids = []
-
-    # idだけ集める
-    results.each do |result|
-      collection_ids << result.post_id
-    end
-
-    # 集めたidで引く
-    @collections = Post.where(id: collection_ids)
+    @collections = Post.where(id: results.pluck(:post_id))
   end
 
   def posts
