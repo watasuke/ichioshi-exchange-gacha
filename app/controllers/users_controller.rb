26a0e10f4d2a5f060a63222dc347bc25be0f28dd
matsurai25 <m.matsurai25@gmail.com>
2018-08-30 04:31:15 +0900

    viewで取得してた処理をControllerに持ってくる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   def collection
-    @collections = Result.where(user_id: current_user.id)
+    results = Result.where(user_id: current_user.id)
+    collection_ids = []
+
+    # idだけ集める
+    results.each do |result|
+      collection_ids << result.post_id
+    end
+
+    # 集めたidで引く
+    @collections = Post.where(id: collection_ids)
   end
 
   def posts
