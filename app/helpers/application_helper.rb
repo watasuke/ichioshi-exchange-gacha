534b472bc41c3202f000e5dd99155e406c9c6ab0
土肥涼介 <watasuke221@gmail.com>
2018-09-02 17:41:13 +0900

    ヘッダーとフッターを整えた

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def document_title
+      if @document_title.present?
+        h(@document_title) + ' | イチオシ交換ガチャ'
+      else
+        'イチオシ交換ガチャ'
+      end
+    end
+
 end
