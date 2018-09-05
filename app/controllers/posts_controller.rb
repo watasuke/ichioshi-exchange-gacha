0a258f656a8df091cde4bc5d3a41162ce2ec1367
土肥涼介 <watasuke221@gmail.com>
2018-09-05 22:25:58 +0900

    投稿に投稿者の画像とTwitter IDをつけたけどコレクションページではまだ(やり方がわからない)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate
   def show
     @post = Post.find_by(slug: params[:slug])
+    @user = User.find_by(id: @post.user_id)
   end
 
   def new
