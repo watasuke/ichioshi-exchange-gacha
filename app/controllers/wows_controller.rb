8925632b62b6e81afd76dc80ff0e9bd1f3e62eda
matsurai25 <m.matsurai25@gmail.com>
2018-09-29 23:22:03 +0900

    受け取るIDが間違っているので修正

--- app/controllers/wows_controller.rb
+++ app/controllers/wows_controller.rb
@@ -2,21 +2,21 @@ class WowsController < ApplicationController
   before_action :set_post
 
   def create
-    @wow = Wow.create(user_id: current_user.id, slug: params[:slug])
-    @wows = Wow.where(slug: params[:slug])
+    @wow = Wow.create(user_id: current_user.id, post_id: @post.id)
+    @wows = Wow.where(post_id: @post.id)
     @post.reload
   end
 
   def destroy
-    wow = Wow.find_by(user_id: current_user.id, slug: params[:slug])
+    wow = Wow.find_by(user_id: current_user.id, post_id: @post.id)
     wow.destroy
-    @wows = Wow.where(slug: params[:slug])
+    @wows = Wow.where(post_id: @post.id)
     @post.reload
   end
 
   private
 
   def set_post
-    @post = Post.find(slug: params[:slug])
+    @post = Post.find(params[:post_id])
   end
 end
